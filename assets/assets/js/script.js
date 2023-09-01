b0ccabde83fd13141bb09cf140266bc54790756d
Ben McCormick <mr.mccormick@Bens-MBP.lan>
2023-08-31 21:10:36 -0700

    Added JS

--- assets/assets/js/script.js
+++ assets/assets/js/script.js
@@ -1,5 +1,3 @@
-// Provides questions and answers for coding quiz //
-
 const questions = [
     {
         question: "Commonly used data types DO NOT include:",
@@ -53,5 +51,153 @@ const questions = [
     },
 ];
 
-const clickStart = document.getElementById('start');
-const timerEl = document.getElementByID('countdown');
+const clickStart = document.getElementById('start-quiz');
+const timerEl = document.getElementById('timer');
+const timeRemaining = 60;
+const questionContainer = document.querySelector('quiz-container');
+
+function timer () {
+    timerEl.textContent = 'Time Remaing: ' + timeRemaining + 's';
+    quizDuration = setInterval(function() {
+        if (timeRemaining > 0) { 
+            adjustTime(-1);
+        } else {
+            endQuiz();
+        }
+    }, 1000);
+}
+
+function adjustTime(amount) {
+    timeRemaining += amount;
+    if (timeRemaining < 0) {
+        timeRemaining = 0;
+    }
+    timerEl.textContent = 'Time Remaing: ' + timeRemaing + 's';
+}
+
+clickStart.onclick = timer; 
+
+function renderQuestion(question) {
+    questionContainer.innerHTML = '';
+
+    const questionHeader = document.createElement('h2');
+    questionHeader.textContent = question.q;
+
+    const questionA = document.createElement('button');
+    answerA.textContent = question.a;
+    answerA.addEventListener ('click', answerClick)
+
+    const questionB = document.createElement('button');
+    answerB.textContent = question.b;
+    answerB.addEventListener ('click', answerClick)
+
+    const questionC = document.createElement('button');
+    answerC.textContent = question.c;
+    answerC.addEventListener ('click', answerClick)
+
+    const questionD = document.createElement('button');
+    answerD.textContent = question.d;
+    answerD.addEventListener ('click', answerClick)
+
+    questionContainer.appendChild(questionHeader);
+    questionContainer.appendChild(answerA);
+    questionContainer.appendChild(answerB);
+    questionContainer.appendChild(answerC);
+    questionContainer.appendChild(answerD);
+};
+
+let currentQuestionIndex = 0;
+let userScore = 0;
+let correctAnswer = questions[currentQuestionIndex].answer;
+const clickHighScores = document.getElementById('high-scores');
+
+const answerClick = function(event) {
+    event.preventDefault();
+    const userAnswer = event.target.textContent;
+    correctAnswer = questions[currentQuestionIndex].answer;
+    const answerDetermination = document.querySelector('.answer-determination');
+
+   
+    if (userAnswer !== correctAnswer) {
+        adjustTime(-10);
+        answerDetermination.textContent = 'Wrong!';
+        //
+    } else {
+        currentQuestionIndex++; // 
+        answerDetermination.textContent = 'Correct!';
+        userScore++;
+        if (currentQuestionIndex >= questions.length) {
+            endQuiz();
+        } else {
+            renderQuestion(questions[currentQuestionIndex]);
+        }
+    }
+};
+
+const quiz = function (event) { 
+    event.preventDefault();
+    resetDisplay();
+    renderQuestion(questions[currentQuestionIndex])
+};
+
+function resetDisplay() {
+    questionContainer.innerHTML = '';
+    document.querySelector('.homepage').style.display = 'none';
+};
+
+function highScores() {
+    const data = localStorage.getItem('object');
+    const getData = JSON.parse;
+    const name = getData.name;
+    const score = getData.score;
+    questionContainer.innerHTML = '';
+    questionContainer.innerHTML = name + '' + score;
+};
+
+clickHighScores.addEventListener('click', () =>
+    { highScores ();
+})
+
+const initials = ''; 
+function endQuizPage () {
+    resetDisplay();
+    timerEl.textContent = '';
+    clearInterval(quizDuration);
+    const endPage = document.createElement ('h2');
+    questionContainer.appendChild(endPage);
+
+    const blank = document.querySelector('answer-determination');
+    blank.innerHTML = '';
+
+    endPage.innerHTML = 'All done! Your final score is ' + 'userScore + .' + 'Enter your initials to save (max 3)';
+
+    const initialBox = document.createElement('input');
+    blank.appendChild(initialBox);
+
+    const submitInitialBtn= document.createElement('button');
+    submitInitialBtn.appendChild= 'Submit';
+    blank.appendChild(submitInitialBtn);
+
+    submitInitialBtn.addEventListener('click', () =>
+    { if (initialBox.value.length === 0) return false;
+        const storeInitials = (...input) =>
+        {
+            const data = JSON.stringify({ "name":input[0], "score":input[1]})
+            localStorage.setItem('object', data)
+        }
+        storeInitials(initialBox.value, userScore);
+
+        const playAgain = document.createElement('button');
+        playAgain.textContent = 'Play Again!'; 
+        blank.appendChild(playAgain);
+
+        playAgain.eventListener('click', () =>
+       {location.reload()
+    })
+    })
+
+    document.querySelector('input').value = '';
+    initialBox.addEventListener('submit', endQuizPage);
+};
+
+clickStart.addEventListener('click', quiz); 
