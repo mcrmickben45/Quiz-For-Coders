9f4c1d2c2d85480fcbc69f0f8ba5bf9fb4a4176b
Ben McCormick <mr.mccormick@Bens-MBP.lan>
2023-08-31 22:32:48 -0700

    Troubleshooting JavaScript

--- assets/assets/js/script.js
+++ assets/assets/js/script.js
@@ -1,63 +1,53 @@
-const questions = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        selection: [
-            "1. Strings",
-            "2. Booleans",
-            "3. Alerts",
-            "4. Numbers"
-        ],
-        answer: "2. Booleans"
+const questions = [{
+        question: 'Commonly used data types DO NOT include:',
+        a: '1. Strings',
+        b: '2. Booleans',
+        c: '3. Alerts',
+        d: '4. Numbers',
+        answer: '3. Alerts',
     },
     {
-        question: "The condition in an if / else statement is enclosed with ____",
-        selection: [
-            "1. Quotes",
-            "2. Curly Brackets",
-            "3. Parenthesis",
-            "4. Square Brackets"
-        ],
-        answer: "3. Parenthesis",
+        question: 'The condition in an if / else statement is enclosed with ____',
+        a: '1. Quotes',
+        b: '2. Curly Brackets',
+        c: '3. Parenthesis',
+        d: '4. Square Brackets',
+        answer: '3. Parenthesis',
     },
     {
-        question: "Arrays in JavaScript can be used to store ____.",
-        selection: [ 
-            "1. Numbers and Strings",
-            "2. Other Arrays",
-            "3. Booleans",
-            "4. All of the Above"
-        ],
-        answer: "4. All of the Above",
+        question: 'Arrays in JavaScript can be used to store ____.',
+        a: '1. Numbers and Strings',
+        b: '2. Other Arrays',
+        c: '3. Booleans',
+        d: '4. All of the Above',
+        answer: '4. All of the Above',
     },    
     {
-        question: "String values must be enclosed within ____ when being assigned to variables",
-        selection: [
-            "1. Commas",
-            "2. Curly Brackets",
-            "3. Quotes",
-            "4. Parenthesis"
-        ],
-        answer: "2. Curly Brackets",
+        question: 'String values must be enclosed within ____ when being assigned to variables',
+        a: '1. Commas',
+        b: '2. Curly Brackets',
+        c: '3. Quotes',
+        d: '4. Parenthesis',
+        answer: '3. Quotes',
     },
     {
-        question: "A very useful tool used during development and debugging for prionting content to the debugger is:",
-        selection: [
-            "1. JavaScript",
-            "2. Terminal/Bash",
-            "3. For Loops",
-            "4. console.log"
-        ],
-        answer: "3. For Loops",
+        question: 'A very useful tool used during development and debugging for prionting content to the debugger is:',
+        a: '1. JavaScript',
+        b:  '2. Terminal/Bash',
+        c: '3. For Loops',
+        d: '4. console.log',
+        answer: '4. console.log',
     },
 ];
 
 const clickStart = document.getElementById('start-quiz');
+
 const timerEl = document.getElementById('timer');
 const timeRemaining = 60;
-const questionContainer = document.querySelector('quiz-container');
+const questionContainer = document.querySelector('#quiz-container');
 
 function timer () {
-    timerEl.textContent = 'Time Remaing: ' + timeRemaining + 's';
+    timerEl.textContent = 'Time Remaining: ' + timeRemaining + 's';
     quizDuration = setInterval(function() {
         if (timeRemaining > 0) { 
             adjustTime(-1);
@@ -72,38 +62,42 @@ function adjustTime(amount) {
     if (timeRemaining < 0) {
         timeRemaining = 0;
     }
-    timerEl.textContent = 'Time Remaing: ' + timeRemaing + 's';
+    timerEl.textContent = 'Time Remaining: ' + timeRemaining + 's';
 }
 
 clickStart.onclick = timer; 
-
-function renderQuestion(question) {
+const renderQuestion = function (question) {
     questionContainer.innerHTML = '';
 
     const questionHeader = document.createElement('h2');
-    questionHeader.textContent = question.q;
+    questionHeader.textContent = questions.question;
 
-    const questionA = document.createElement('button');
-    answerA.textContent = question.a;
+    const answerA = document.createElement('button');
+    answerA.textContent = question.c;
     answerA.addEventListener ('click', answerClick)
 
-    const questionB = document.createElement('button');
-    answerB.textContent = question.b;
+    const answerB = document.createElement('button');
+    answerB.textContent = question.c;
     answerB.addEventListener ('click', answerClick)
 
-    const questionC = document.createElement('button');
-    answerC.textContent = question.c;
+    const answerC = document.createElement('button');
+    answerC.textContent = question.d;
     answerC.addEventListener ('click', answerClick)
 
-    const questionD = document.createElement('button');
-    answerD.textContent = question.d;
+    const answerD = document.createElement('button');
+    answerD.textContent = question.c;
     answerD.addEventListener ('click', answerClick)
+
+    const answerE = document.createElement('button');
+    answerE.textContent = question.d;
+    answerE.addEventListener ('click', answerClick)
 
     questionContainer.appendChild(questionHeader);
     questionContainer.appendChild(answerA);
     questionContainer.appendChild(answerB);
     questionContainer.appendChild(answerC);
     questionContainer.appendChild(answerD);
+    questionContainer.appendChild(answerE);
 };
 
 let currentQuestionIndex = 0;
@@ -115,43 +109,38 @@ const answerClick = function(event) {
     event.preventDefault();
     const userAnswer = event.target.textContent;
     correctAnswer = questions[currentQuestionIndex].answer;
-    const answerDetermination = document.querySelector('.answer-determination');
+    const answerDetermination = document.querySelector('#answer-determination');
 
-   
     if (userAnswer !== correctAnswer) {
         adjustTime(-10);
         answerDetermination.textContent = 'Wrong!';
-        //
-    } else {
-        currentQuestionIndex++; // 
-        answerDetermination.textContent = 'Correct!';
-        userScore++;
-        if (currentQuestionIndex >= questions.length) {
-            endQuiz();
-        } else {
-            renderQuestion(questions[currentQuestionIndex]);
-        }
-    }
+        currentQuestionIndex++; 
+    if (currentQuestionIndex >= questions.length) {
+            endQuizPage();
+        } else {renderQuestion(questions[currentQuestionIndex])};
+
+}
 };
 
 const quiz = function (event) { 
     event.preventDefault();
     resetDisplay();
-    renderQuestion(questions[currentQuestionIndex])
+    renderQuestion(questions[currentQuestionIndex]);
+    timer();
 };
 
 function resetDisplay() {
     questionContainer.innerHTML = '';
-    document.querySelector('.homepage').style.display = 'none';
+    document.getElementById('homepage').style.display = 'none';
 };
 
 function highScores() {
     const data = localStorage.getItem('object');
-    const getData = JSON.parse;
+    const getData = JSON.parse(data);
     const name = getData.name;
     const score = getData.score;
     questionContainer.innerHTML = '';
-    questionContainer.innerHTML = name + '' + score;
+    questionContainer.innerHTML = name + ' ' + score;
 };
 
 clickHighScores.addEventListener('click', () =>
@@ -175,12 +164,12 @@ function endQuizPage () {
     blank.appendChild(initialBox);
 
     const submitInitialBtn= document.createElement('button');
-    submitInitialBtn.appendChild= 'Submit';
+    submitInitialBtn.textContent= 'Submit';
     blank.appendChild(submitInitialBtn);
 
     submitInitialBtn.addEventListener('click', () =>
     { if (initialBox.value.length === 0) return false;
-        const storeInitials = (...input) =>
+        const storeInitials = (input) =>
         {
             const data = JSON.stringify({ "name":input[0], "score":input[1]})
             localStorage.setItem('object', data)
@@ -191,13 +180,13 @@ function endQuizPage () {
         playAgain.textContent = 'Play Again!'; 
         blank.appendChild(playAgain);
 
-        playAgain.eventListener('click', () =>
+        playAgain.addEventListener('click', () =>
        {location.reload()
     })
     })
 
     document.querySelector('input').value = '';
-    initialBox.addEventListener('submit', endQuizPage);
+    initialBox.addEventListener('submit', change);
 };
 
 clickStart.addEventListener('click', quiz); 
